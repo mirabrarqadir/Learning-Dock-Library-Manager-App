16b8b6187884526bb0ba646c2abb8ee5dab47387
mirabrarqadir <mirabrarqadir@gmail.com>
2026-04-30 12:27:29 +0530

    WhatsApp reminder text final edit

--- public/app.js
+++ public/app.js
@@ -92,7 +92,7 @@ function makeMembershipReminderMessage(student) {
     "",
     "Thank you.",
     "",
-    "___This is an auto sent reminder text by the App.___"
+    "```This is an auto sent reminder text by the App```"
   ].join("\n");
 }
 
